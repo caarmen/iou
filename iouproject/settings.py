c308d29ee99b21144e81c186dd36dedda5fb3937
Carmen Alvarez <carmen@rmen.ca>
2023-10-29 23:57:59 +0100

    Move some security-related settings variables to .env.

--- iouproject/settings.py
+++ iouproject/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hqe)^0cogtbvg)a_#&^-pfoduj6rlt*l_p$cwsfm1_2)5(d8l&"
+SECRET_KEY = environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = environ["ALLOWED_HOSTS"].split(",")
 
 
 # Application definition
