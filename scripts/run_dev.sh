a6a000d3e988fe2f1d26192a75d5b2078a257f9b
Carmen Alvarez <carmen@rmen.ca>
2023-11-01 00:16:25 +0100

    Style the main page.

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,5 +1,6 @@
 export DEBUG=true
 python manage.py migrate && \
 python manage.py createadminuser && \
+pushd iou && django-admin makemessages --locale en_US --locale fr && popd && \
 django-admin compilemessages && \
 python manage.py runserver 8000
