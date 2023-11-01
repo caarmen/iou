69910d40f925ac346073d00622decd5622edcad3
Carmen Alvarez <carmen@rmen.ca>
2023-11-01 16:19:06 +0100

    Use babel to format amounts with currency info.
    Also make "optional" in the description hint lowercase (and inthe correct language!)

--- iou/templatetags/iou_filters.py
+++ iou/templatetags/iou_filters.py
@@ -1,7 +1,10 @@
 import os
 
+from babel.numbers import format_compact_currency
 from django import template
+from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation import to_locale
 
 from iou.models import Debt, Person
 from iou.service import NetDebt
@@ -23,18 +26,28 @@ def debt(value: Debt) -> str:
     if value.description:
         return _("debt_list_item_with_description").format(
             person(value.debtor),
-            value.amount,
+            money(value.amount),
             value.description,
         )
 
     return _("debt_list_item").format(
         person(value.debtor),
-        value.amount,
+        money(value.amount),
     )
 
 
 @register.filter(name="net_debt")
 def net_debt(value: NetDebt) -> str | None:
     return (
-        _("who_owes_what").format(person(value.debtor), value.amount) if value else None
+        _("who_owes_what").format(person(value.debtor), money(value.amount))
+        if value
+        else None
+    )
+
+
+def money(value) -> str:
+    return format_compact_currency(
+        number=value,
+        currency=os.environ["CURRENCY_CODE"],
+        locale=to_locale(get_language()),
     )
