fbb64650f9eca3c87b78550438c07d995d3259d2
Carmen Alvarez <carmen@rmen.ca>
2023-11-05 23:52:48 +0100

    Add login/logout functionality, and make the main pages require login.

--- iou/tests/fixtures.py
+++ iou/tests/fixtures.py
@@ -1,7 +1,20 @@
 import pytest
+from django.contrib.auth.models import User
 from django.test import Client
 
 
 @pytest.fixture()
-def client():
-    return Client()
+def test_account_password():
+    return "secret"
+
+
+@pytest.fixture()
+def user(test_account_password):
+    return User.objects.create_user(username="fred", password=test_account_password)
+
+
+@pytest.fixture()
+def client(user: User, test_account_password: str):
+    c = Client()
+    c.login(username=user.username, password=test_account_password)
+    return c
