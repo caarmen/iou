6b73ad3e8a475ecf56c0f45f004a402382847fd9
Carmen Alvarez <carmen@rmen.ca>
2023-11-11 16:05:39 +0100

    Move the login/logout urls to the iou app

--- iou/apps.py
+++ iou/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.urls import reverse
 
 
 class IouConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "iou"
+
+    def ready(self):
+        from django.conf import settings
+
+        settings.LOGIN_URL = reverse("login")
--- iou/urls.py
+++ iou/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -5,6 +6,16 @@ from . import views
 urlpatterns = [
     # ex: /iou/
     path("", views.index, name="index"),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(next_page="iou"),
+        name="login",
+    ),
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(next_page="login"),
+        name="logout",
+    ),
     path("delete/<int:debt_id>/", views.delete, name="delete"),
     path("site.webmanifest", views.webmanifest, name="webmanifest"),
 ]
