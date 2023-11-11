7facbbcdbe5483eadfe0f3c50065c4952dfc72cf
Carmen Alvarez <carmen@rmen.ca>
2023-11-11 16:10:46 +0100

    Add check in codecheck.sh that we don't have any translations to reformat.

--- scripts/codecheck.sh
+++ scripts/codecheck.sh
@@ -5,4 +5,10 @@ do
   ruff check --fix --exit-non-zero-on-fix $project || error=$?
   isort --profile black $project || error=$?
 done
+
+# Make sure we don't have any translation updates
+pushd iou && django-admin makemessages --locale en_US --locale fr && popd
+django-admin compilemessages || error=$?
+git diff --exit-code || error=$?
+
 exit $error
