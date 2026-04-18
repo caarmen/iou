eb811f5a61c126ded201d28dcf5172c8e6382f6f
Carmen Alvarez <carmen@rmen.ca>
2026-04-18 14:58:57 +0200

    libs: Update dev dependencies.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,5 @@
 rm -rf reports
 python -m manage collectstatic --noinput
-python -m pytest --cov=iou  --cov-report=xml --cov-report=html --junitxml="reports/junit.xml" iou/tests
+python -m pytest --cov=iou  --cov-report=xml --cov-report=html --junitxml="reports/junit.xml"
 mkdir -p reports
 mv coverage.xml htmlcov reports/.
