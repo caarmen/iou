d29a814bf043fda44da446e5d4bb9c3fa9da820b
Carmen Alvarez <carmen@rmen.ca>
2026-04-19 21:23:44 +0200

    feat: Define the Credential model in the passkeys app.

--- passkeys/admin.py
+++ passkeys/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+
+from passkeys.models import Credential
+
+
+class CredentialAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "credential_id",
+        "public_key",
+        "sign_count",
+        "created_at",
+    )
+
+
+admin.site.register(Credential, CredentialAdmin)
