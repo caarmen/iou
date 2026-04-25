3fa7e6aff6cb12d143e718651ad13ceb197b24a2
Carmen Alvarez <carmen@rmen.ca>
2026-04-26 00:13:15 +0200

    test: Adjust code coverage config.
    * Include iouproject in test coverage (except for wsgi.py and asgi.py files).
    * Exclude tests from test coverage.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,5 @@
 rm -rf reports
 python -m manage collectstatic --noinput
-python -m pytest --cov=iou  --cov-report=xml --cov-report=html --junitxml="reports/junit.xml"
+python -m pytest --cov=iou --cov=iouproject --cov-report=xml --cov-report=html --junitxml="reports/junit.xml"
 mkdir -p reports
 mv coverage.xml htmlcov reports/.
